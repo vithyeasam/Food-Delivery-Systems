8249b6a9ea0a3e8745d9e12156d2997c2e1e1dbc
vithyeasam <sam.vithyea@gmail.com>
2024-07-28 15:17:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 "use strict";
 function main(lines) { // lines: Array<string>
-  /**
-   * このコードは標準入力と標準出力を用いたサンプルコードです。
-   * このコードは好きなように編集・削除してもらって構いません。
-   *
-   * This is a sample code to use stdin and stdout.
-   * You can edit and even remove this code as you like.
-  */
 
 let jsonData = [];
 let jsonStore = [];
